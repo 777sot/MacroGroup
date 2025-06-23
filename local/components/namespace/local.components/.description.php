6fd6d0471e0ed9674fbf820fb2766c01c332c6b4
Dmitrii Klimenko <777soten@mail.ru>
2025-06-23 21:11:21 +0300

    Add CI/CD workflow

--- local/components/namespace/local.components/.description.php
+++ local/components/namespace/local.components/.description.php
@@ -1,6 +1,7 @@
 <?php
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 
+
 $arComponentDescription = [
     'NAME' => 'Список сделок',
     'DESCRIPTION' => 'Компонент для отображения списка сделок с возможностью сортировки',
